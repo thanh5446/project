2e7496b1f7d53097d56fc61552dabf52333c73df
thanh5446 <thanhtngcd201711@fpt.edu.vn>
2024-11-05 21:05:37 +0700

    update footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -9,20 +9,22 @@ const Footer = () => {
         <div className="row">
           {/* Customer Support Section */}
           <div className="col-md-3">
-            <h6>Hỗ trợ khách hàng</h6>
+            <h6>Customer Service</h6>
             <p>
               Hotline: <strong>1900-6035</strong>
               <br />
-              <small>(1000 đ/phút, 8-21h kể cả T7, CN)</small>
+              <small>
+                (1000 VND/min, 8 AM - 9 PM, including Saturdays and Sundays.)
+              </small>
             </p>
           </div>
 
           {/* About Section */}
           <div className="col-md-2">
-            <h6>Về Teetech</h6>
+            <h6>About Teetech</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#">Giới thiệu Teetech</a>
+                <a href="#">Introduction to Teetech</a>
               </li>
               <li>
                 <a href="#">Teetech Blog</a>
@@ -32,16 +34,18 @@ const Footer = () => {
 
           {/* Partnerships Section */}
           <div className="col-md-2">
-            <h6>Hợp tác và liên kết</h6>
+            <h6>Collaboration and Alliances</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#">Quy chế hoạt động Sàn GDTMĐT</a>
+                <a href="#">
+                  Operational Regulations of the E-commerce Platform
+                </a>
               </li>
               <li>
-                <a href="#">Bán hàng cùng Teetech</a>
+                <a href="#">Sell with Teetech</a>
               </li>
             </ul>
-            <h6 className="mt-3">Chứng nhận bởi</h6>
+            <h6 className="mt-3">Accredited by</h6>
             <img
               src="https://frontend.tikicdn.com/_desktop-next/static/img/footer/bo-cong-thuong-2.png"
               alt="Certification 1"
@@ -57,7 +61,7 @@ const Footer = () => {
 
           {/* Payment Methods Section */}
           <div className="col-md-2">
-            <h6>Phương thức thanh toán</h6>
+            <h6>Payment Method</h6>
             <div className="d-flex flex-wrap">
               <img
                 src="https://developers.momo.vn/v3/vi/assets/images/square-8c08a00f550e40a2efafea4a005b1232.png"
@@ -66,13 +70,13 @@ const Footer = () => {
                 style={{ width: "40px", height: "auto" }} // Adjust width as needed
               />
             </div>
-            <h6 className="mt-3">Dịch vụ giao hàng</h6>
+            <h6 className="mt-3">Delivery Service</h6>
             <p>Teetech</p>
           </div>
 
           {/* Social Media and App Download */}
           <div className="col-md-3">
-            <h6>Kết nối với chúng tôi</h6>{" "}
+            <h6>Connect with Us</h6>{" "}
             <a
               href="https://www.facebook.com/profile.php?id=100007858573546&locale=vi_VN"
               className="me-2"
